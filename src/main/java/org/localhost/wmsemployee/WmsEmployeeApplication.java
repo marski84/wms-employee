df0cd4d74dbd37f2e4d4b3c54a5c86b542af7589
marski84 <marski705@gmail.com>
2025-02-25 18:13:32 +0100

    initial commit

---
+++ src/main/java/org/localhost/wmsemployee/WmsEmployeeApplication.java
@@ -0,0 +1,13 @@
+package org.localhost.wmsemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/localhost/wmsemployee/WmsEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.localhost.wmsemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
